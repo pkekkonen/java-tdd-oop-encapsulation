2f27e8f9646f83a78ca147663c5bb812471e9612
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-17 10:55:05 +0100

    setInvalidColourTest passed

--- src/main/java/com/booleanuk/core/RemoteControlledCar.java
+++ src/main/java/com/booleanuk/core/RemoteControlledCar.java
@@ -5,12 +5,17 @@ import java.util.Arrays;
 
 public class RemoteControlledCar {
     private String colour;
+    private final ArrayList<String> validColours;
 
     public RemoteControlledCar() {
-
+        colour = "white";
+        validColours = new ArrayList<>(Arrays.asList("orange","pink", "black"));
     }
 
     public boolean setColour(String colour) {
+        if(!validColours.contains(colour)) {
+            return false;
+        }
         this.colour = colour;
         return true;
     }
